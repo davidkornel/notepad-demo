e3619a90fd41f72cff4e6d66ddeb7a186132b567
Kornel David <konel1020@gmail.com>
2023-11-28 13:55:54 +0100

    initial commit:
    go.mod/sum added
    main.go with initial GIN integration

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-logr/logr"
+)
+
+func main() {
+	logger := logr.Logger{}
+	setupLog := logger.WithName("setupLog")
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := router.Run()
+	if err != nil {
+		return
+	} // listen and serve on 0.0.0.0:8080
+	setupLog.Info("Server set up succesfully, serving on http://localhost:8080")
+}
