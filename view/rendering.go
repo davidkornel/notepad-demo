91a8303053a884995b49dce9b7419e60c6c88e5e
Kornel David <konel1020@gmail.com>
2023-11-29 01:57:35 +0100

    fix: rendering rework
    might need some work

--- view/rendering.go
+++ view/rendering.go
@@ -3,6 +3,7 @@ package view
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 // Render one of HTML, JSON or CSV based on the 'Accept' header of the request
@@ -11,13 +12,12 @@ import (
 func Render(c *gin.Context, data gin.H, templateName string) {
 	// Authentication should be checked here
 
-	switch c.Request.Header.Get("Accept") {
-	case "application/json":
+	acceptHeader := c.Request.Header.Get("Accept")
+
+	switch {
+	case strings.Contains(acceptHeader, "application/json"):
 		// Respond with JSON
 		c.JSON(http.StatusOK, data["payload"])
-	case "application/xml":
-		// Respond with XML
-		c.XML(http.StatusOK, data["payload"])
 	default:
 		// Respond with HTML
 		c.HTML(http.StatusOK, templateName, data)
