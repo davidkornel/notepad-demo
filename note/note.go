da928fa41818e3c780d5467ace371eca0de177c7
Kornel David <konel1020@gmail.com>
2023-11-30 15:14:27 +0100

    feat: Edit note route

--- note/note.go
+++ note/note.go
@@ -28,11 +28,41 @@ func (r *Routes) RegisterRoutes(router *gin.Engine) {
 		noteRoutes.GET("/all", r.showAllNotes)
 		noteRoutes.POST("/create", r.createNote)
 		//noteRoutes.GET("/view/:id", viewNote)
-		//noteRoutes.POST("/edit/:id", editNote)
+		noteRoutes.POST("/edit/:id", r.editNote)
 		//noteRoutes.DELETE("/delete/:id", deleteNote)
 	}
 	r.logger.WithName("RegisterRoutes").V(1).Info("routes registered for /note")
 
+}
+
+func (r *Routes) editNote(c *gin.Context) {
+	log := r.logger.WithName("editNote")
+
+	id, err := uuid.FromString(c.Param("id"))
+	if err != nil {
+		log.Error(err, "error happened while fetching id")
+	}
+	log.Info("NoteID fetched from url", "noteid", id.String())
+
+	var noteData POSTData
+
+	if err := c.ShouldBindJSON(&noteData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	note := POSTData2Note(id, noteData)
+
+	err = r.editNoteInDB(note)
+	if err != nil {
+		log.Error(err, "error happened while updating note in DB")
+		return
+	}
+	payload := note2Map(note)
+
+	view.Render(c, gin.H{
+		"payload": payload,
+	}, "")
 }
 
 func (r *Routes) createNote(c *gin.Context) {
@@ -65,17 +95,20 @@ func (r *Routes) createNote(c *gin.Context) {
 func (r *Routes) showAllNotes(c *gin.Context) {
 	log := r.logger.WithName("showAllNotes")
 
-	var notes []RenderNote
+	var renderNotes []RenderNote
 
-	notes = append(notes, RenderNote{
-		Group:  "testgroup",
-		NoteID: uuid.NewV4().String(),
-		Title:  "Note",
-		Text:   "Blandit tempus porttitor aasfs. Integer posuere erat a ante venenatis.",
-	})
+	for _, n := range r.fetchAllNoteFromDB() {
+		renderNotes = append(renderNotes, RenderNote{
+			Group:  n.Group,
+			NoteID: n.NoteID.String(),
+			Title:  n.Title,
+			Text:   n.Text,
+		})
+	}
+
 	view.Render(c, gin.H{
 		"title": "demo",
-		"notes": notes,
+		"notes": renderNotes,
 	}, "note-all.html")
-	log.V(1).Info("notes have been fetched", "notes", notes)
+	log.V(1).Info("Notes have been fetched", "notes", renderNotes)
 }
