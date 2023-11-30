e6f59972e5259bb3e60ec7801390928be44c116a
Kornel David <konel1020@gmail.com>
2023-11-30 15:14:02 +0100

    feat: Database upgrade
    * fetch all notes
    * edit single note

--- note/db.go
+++ note/db.go
@@ -2,8 +2,32 @@ package note
 
 import (
 	"context"
+	"errors"
 	"github.com/davidkornel/notepad-demo/config"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func (r *Routes) fetchAllNoteFromDB() []Note {
+	log := r.logger.WithName("fetchAllNoteFromDB")
+
+	var results []Note
+	log.Info("Trying to fetch all notes from database")
+	coll := r.dbClient.Database(config.DefaultDatabaseTableName).Collection(config.DefaultDatabaseNoteCollectionName)
+
+	cursor, err := coll.Find(context.TODO(), bson.D{})
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		log.Info("MongoDB table is empty, no document found")
+		return nil
+	}
+	if err != nil {
+		log.Error(err, "error happened while finding notes from DB")
+	}
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		panic(err)
+	}
+	return results
+}
 
 func (r *Routes) saveNoteIntoDB(note Note) error {
 	log := r.logger.WithName("saveNoteIntoDB")
@@ -17,5 +41,30 @@ func (r *Routes) saveNoteIntoDB(note Note) error {
 		return err
 	}
 	log.Info("Successfully saved note into db", "note", note)
+	return nil
+}
+
+func (r *Routes) editNoteInDB(note Note) error {
+	log := r.logger.WithName("saveNoteIntoDB")
+
+	log.Info("Trying to save note into database", "note", note)
+	coll := r.dbClient.Database(config.DefaultDatabaseTableName).Collection(config.DefaultDatabaseNoteCollectionName)
+
+	update := bson.D{{"$set", bson.D{
+		{"text", note.Text},
+		{"group", note.Group},
+	}}}
+	result, err := coll.UpdateOne(context.TODO(), bson.D{{"noteid", note.NoteID}}, update)
+	if err != nil {
+		return err
+	}
+
+	if result.MatchedCount == 1 {
+		log.Info("matched and updated successfully an existing note in DB")
+	}
+	if result.UpsertedCount != 0 {
+		log.Info("upserted a new note", "id", note.NoteID, "title", note.Title)
+	}
+
 	return nil
 }
